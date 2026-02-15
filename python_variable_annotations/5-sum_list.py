4c3d98bb0d10cf56437868062eb97c253ad263f4
SabSuf <12734@holbertonstudents.com>
2026-02-15 18:27:18 +0400

    Update 5-sum_list.py

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 """This is about annotation"""
+from typing import List
 
 
-def sum_list(input_list: list[float]) -> float:
+def sum_list(input_list: List[float]) -> float:
     """Funct about the summary"""
     return sum(input_list)
