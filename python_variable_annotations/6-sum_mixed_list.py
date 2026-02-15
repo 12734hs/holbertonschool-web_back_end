cea89181e748c9d261e810a86060fab0b7ef034f
SabSuf <12734@holbertonstudents.com>
2026-02-15 18:30:03 +0400

    Update 6-sum_mixed_list.py

--- python_variable_annotations/6-sum_mixed_list.py
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 """This is about annotation"""
-from typing import List
+from typing import List, Union
 
 
-def sum_mixed_list(mxd_list: List[float, int]) -> float:
+def sum_mixed_list(mxd_list: List[Union[float, int]]) -> float:
     """Funct about the summary"""
     return sum(mxd_list)
