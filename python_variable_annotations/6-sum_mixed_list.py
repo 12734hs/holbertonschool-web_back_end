5a0c2ae188a830adbe0e2968763b35604fe97fbf
SabSuf <12734@holbertonstudents.com>
2026-02-15 18:30:41 +0400

    Update 6-sum_mixed_list.py

--- python_variable_annotations/6-sum_mixed_list.py
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -3,6 +3,6 @@
 from typing import List, Union
 
 
-def sum_mixed_list(mxd_list: List[Union[float, int]]) -> float:
+def sum_mixed_list(mxd_list: List[Union[int, float]]) -> float:
     """Funct about the summary"""
     return sum(mxd_list)
