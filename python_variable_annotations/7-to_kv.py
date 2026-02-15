23b50757d56eab9a2b4fe5c67d74347bfde11107
SabSuf <12734@holbertonstudents.com>
2026-02-15 18:38:54 +0400

    Update 7-to_kv.py

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 """MESSIIIIIIIIIII"""
-from typing import List, Union
+from typing import Union, Tuple
 
 
-def to_kv(k: str, v:Union[int, float]) -> tuple[str, float]:
+def to_kv(k: str, v:Union[int, float]) -> Tuple[str, float]:
     """THis is about the annotation"""
     v_square = v ** 2
     return (k, v_square)
