6b5b14ee0f2d7d50d3cbb80171c80da7e78233ea
SabSuf <12734@holbertonstudents.com>
2026-02-15 18:45:47 +0400

    Update 8-make_multiplier.py

--- python_variable_annotations/8-make_multiplier.py
+++ python_variable_annotations/8-make_multiplier.py
@@ -6,7 +6,6 @@ from typing import Callable
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
     """DEF YU"""
 
-    
     def multiply(x: int) -> int:
         return x * multiplier
 
