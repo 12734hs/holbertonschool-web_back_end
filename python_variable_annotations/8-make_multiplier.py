f72bcdaf3042779caa20800fa779ccde48178ae1
SabSuf <12734@holbertonstudents.com>
2026-02-15 18:44:49 +0400

    Create 8-make_multiplier.py

---
+++ python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+'''MESSSIIIII'''
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """DEF YU"""
+
+    
+    def multiply(x: int) -> int:
+        return x * multiplier
+
+    return multiply
