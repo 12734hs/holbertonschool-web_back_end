3662c60b478cd1681ffd4c6967897e20734c20f1
SabSuf <12734@holbertonstudents.com>
2026-02-15 18:28:54 +0400

    Create 6-sum_mixed_list.py

---
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""This is about annotation"""
+from typing import List
+
+
+def sum_mixed_list(mxd_list: List[float, int]) -> float:
+    """Funct about the summary"""
+    return sum(mxd_list)
