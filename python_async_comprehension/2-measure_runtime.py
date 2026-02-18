0858896071b3644384e34994e6d011aa2c9787ef
Sabir <12734@holbertonstudents.com>
2026-02-18 11:23:36 +0400

    Uodated

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -8,12 +8,8 @@ async_comprehension = __import__('1-async_comprehension').async_comprehension
 async def measure_runtime() -> float:
     """YEAHHHHHHHHHHHHHHHHHHHHHHH"""
     start_time = time.perf_counter()
-    await gather(
-                         async_comprehension(),
-                         async_comprehension(),
-                         async_comprehension(),
-                         async_comprehension()
-                        )
+    tasks = [async_comprehension() for _ in range(4)]
+    await gather(*tasks)
     end_time = time.perf_counter()
     final_time = end_time - start_time
     return final_time
