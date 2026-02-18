34d0f3ff1413b9dda6ab7193c28fabb17a680c28
Sabir <12734@holbertonstudents.com>
2026-02-18 11:16:28 +0400

    Updated

---
+++ python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""We are going to check time out"""
+import time
+import asyncio
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime():
+    """YEAHHHHHHHHHHHHHHHHHHHHHHH"""
+    start_time = time.perf_counter()
+    await asyncio.gather(async_comprehension, async_comprehension, async_comprehension, async_comprehension)
+    end_time = time.perf_counter()
+    final_time = end_time - start_time
+    return final_time
