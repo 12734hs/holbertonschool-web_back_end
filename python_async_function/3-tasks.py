266d40afb76a02c2ee7fa8d70d705ba4424d1983
Sabir <12734@holbertonstudents.com>
2026-02-18 10:34:45 +0400

    Updated

--- python_async_function/3-tasks.py
+++ python_async_function/3-tasks.py
@@ -4,6 +4,6 @@ import asyncio
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-def task_wait_random(max_delay):
+def task_wait_random(max_delay: int) -> asyncio.Task:
     """I will try thi time do that max brief"""
     return asyncio.create_task(wait_random(max_delay))
