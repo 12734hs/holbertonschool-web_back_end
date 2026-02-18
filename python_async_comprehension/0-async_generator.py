f8b34af65afee555f3891168570a9ca88a81518c
Sabir <12734@holbertonstudents.com>
2026-02-18 11:04:17 +0400

    Updated

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -2,9 +2,10 @@
 """THis is async, so let do it guys, python is awesome , c is love"""
 import asyncio
 import random
+from typing import Generator
 
 
-async def async_generator():
+async def async_generator() -> Generator[float, None, None]:
     for i in range(10):
         await asyncio.sleep(1)
         number: float = random.uniform(0, 10)
