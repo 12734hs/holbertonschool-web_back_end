7aa58a530d90df60411c93a2292d3eb559b251ac
Sabir <12734@holbertonstudents.com>
2026-02-18 09:48:48 +0400

    Updated

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python3
 import asyncio
 import random
-from typing import Union
 """THis is async"""
 
-async def wait_random(max_delay: Union[int, float]) -> float:
+async def wait_random(max_delay: float) -> float:
     """This is async func"""
     result: float = random.uniform(0, max_delay)
     await asyncio.sleep(result)
