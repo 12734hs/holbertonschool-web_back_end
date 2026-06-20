5c0a396998daec44a24f09f4a8df15748c44746a
Sabir <12734@holbertonstudents.com>
2026-06-20 22:32:49 +0400

    Created files for the task 2, UNITTESTS

---
+++ unittests_in_js/2-calcul.test.js
@@ -0,0 +1,20 @@
+let assert = require('chai')
+let calculateNumber = require('./1-calcul.js')
+
+describe("calculateNumber", () => {
+    it('sum', () => {
+        expect(calculateNumber("SUM", 1.2, 1.9)).to.equal(3)
+    });
+    it('subtrack', () => {
+        expect(calculateNumber("SUBTRACT", 3.2, 1.9)).to.equal(1)
+    });
+    it('subtrack by 0', () => {
+        expect(calculateNumber("SUBTRACT", 0.2, 5.7)).to.equal(-6)
+    });
+    it('divide', () => {
+        expect(calculateNumber("DIVIDE", 7.9, 1.7)).to.equal(4)
+    });
+    it('divide by zero', () => {
+        expect(calculateNumber("DIVIDE", 7.9, 0.2)).to.be.a("string").and.equal("Error")
+    });
+});
