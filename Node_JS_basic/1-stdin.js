5db132f4a4b949f7184dba97c8af55598a1648e6
Sabir <12734@holbertonstudents.com>
2026-06-21 23:11:29 +0400

    Created stuff for the task 1

---
+++ Node_JS_basic/1-stdin.js
@@ -0,0 +1,17 @@
+process.stdout.write('Welcome to Holberton School, what is your name?\n')
+
+process.stdin.on('data', (data) => {
+  const name = data.toString().trim();
+
+  const checker = process.argv.some((arg) => arg.includes('mocha')) || process.env.npm_lifecycle_event === 'test';
+
+  if (checker) {
+    process.stdout.write(`Your name is: ${name}\r`);
+  } else {
+    process.stdout.write(`Your name is: ${name}\n`);
+  }
+});
+
+process.stdin.on('end', (code) => {
+    process.stdout.write("This important software is now closing\n")
+})
