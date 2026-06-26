90ccf3d60696c15caaff7e201cbf3db2ee587806
Sabir <12734@holbertonstudents.com>
2026-06-26 12:22:02 +0400

    Visual Fixes Num2

--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -3,11 +3,11 @@ const PORT = 1245;
 const http = require('http');
 
 const app = http.createServer((req, res) => {
-  res.writeHead(200, {'Content-Type': 'text/plain'} );
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.end('Hello Holberton School!');
-})
+});
 app.listen(PORT, 'localhost', () => {
-  console.log(`Server is executed at the loclhost:${PORT}`)
-})
+  console.log(`Server is executed at the loclhost:${PORT}`);
+});
 
 module.exports = app;
