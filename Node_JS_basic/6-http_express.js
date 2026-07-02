6163c932749cbbbe4c84f22c986fd3f7f0db795d
Sabir <12734@holbertonstudents.com>
2026-07-02 16:51:57 +0400

    Created stuff for the task 6

---
+++ Node_JS_basic/6-http_express.js
@@ -0,0 +1,12 @@
+const PORT = 1245;
+const express = require('express')
+
+const app = express()
+
+app.get("/", (request, response) => {
+    response.send("Hello Holberton School!")
+})
+
+app.listen(PORT);
+
+module.exports = app;
