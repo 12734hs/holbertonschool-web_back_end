9c26785da9515dd09bfc1dd85456af388e1f9f45
Sabir <12734@holbertonstudents.com>
2026-07-02 16:54:24 +0400

    Some Visual Fixes

--- Node_JS_basic/6-http_express.js
+++ Node_JS_basic/6-http_express.js
@@ -1,11 +1,11 @@
 const PORT = 1245;
-const express = require('express')
+const express = require('express');
 
-const app = express()
+const app = express();
 
-app.get("/", (request, response) => {
-    response.send("Hello Holberton School!")
-})
+app.get('/', (request, response) => {
+  response.send('Hello Holberton School!');
+});
 
 app.listen(PORT);
 
