447b3a491d057c9a4f201c6d203924126a1bb287
Sabir <12734@holbertonstudents.com>
2026-07-02 16:58:56 +0400

    Stuff for the task 7

---
+++ Node_JS_basic/7-http_express.js
@@ -0,0 +1,22 @@
+const PORT = 1245;
+const express = require('express');
+const dbName = require('./3-read_file_async')
+const app = express();
+
+app.get('/', (request, response) => {
+  response.send('Hello Holberton School!');
+});
+
+app.get('/students', (request, response) => {
+  countStudents(dbName)
+      .then((result) => {
+        response.status(200).send(`This is the list of our students\n${result}`);
+      })
+      .catch(() => {
+        response.status(500).send('This is the list of our students\nCannot load the database');
+      });
+})
+
+app.listen(PORT);
+
+module.exports = app;
