a8334d6b7f551d36ada15b511f41544f049c9b6c
Sabir <12734@holbertonstudents.com>
2026-07-02 13:52:44 +0400

    Some Visaul Fixes

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -5,26 +5,25 @@ const countStudents = require('./3-read_file_async');
 const dbName = process.argv[2];
 
 const app = http.createServer((req, res) => {
-    if (req.url === '/') {
-      res.writeHead(200, { 'Content-Type': 'text/plain' });
-      res.end('Hello Holberton School!');}
-    else if (req.url === '/students') {
-      res.writeHead(200, { 'Content-Type': 'text/plain' });
-      countStudents(dbName)
-        .then((result) => {
-          res.end(`This is the list of our students\n${result}`);
-        })
-        .catch(() => {
-          res.statusCode = 500;
-          res.end('This is the list of our students\nCannot load the database');
-        });
-    }
-    else {
-      res.writeHead(404);
-      res.end();
-    }
+  if (req.url === '/') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello Holberton School!');}
+  else if (req.url === '/students') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    countStudents(dbName)
+      .then((result) => {
+        res.end(`This is the list of our students\n${result}`);
+    })
+      .catch(() => {
+        res.statusCode = 500;
+        res.end('This is the list of our students\nCannot load the database');
+    });
+}
+  else {
+    res.writeHead(404);
+    res.end();}
 });
 
-app.listen(PORT)
+app.listen(PORT);
 
 module.exports = app;
