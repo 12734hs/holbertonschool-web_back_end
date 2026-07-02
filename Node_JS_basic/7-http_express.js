54925c57dfc3188b277a6521ca8ffd4c53ea6d37
Sabir <12734@holbertonstudents.com>
2026-07-02 17:05:33 +0400

    Some Visual Stuff

--- Node_JS_basic/7-http_express.js
+++ Node_JS_basic/7-http_express.js
@@ -2,7 +2,6 @@ const PORT = 1245;
 const express = require('express');
 const countStudents = require('./3-read_file_async');
 const app = express();
-
 const dbName = process.argv[2];
 
 app.get('/', (request, response) => {
@@ -14,9 +13,9 @@ app.get('/students', (request, response) => {
     .then((result) => {
       response.status(200).send(`This is the list of our students\n${result}`);
     })
-      .catch(() => {
-        response.status(500).send('This is the list of our students\nCannot load the database');
-      });
+    .catch(() => {
+      response.status(500).send('This is the list of our students\nCannot load the database');
+    });
 });
 
 app.listen(PORT);
