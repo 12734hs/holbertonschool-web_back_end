3fb98af296ed249065cee480321ab9158fa83bb0
Sabir <12734@holbertonstudents.com>
2026-07-02 14:04:15 +0400

    Trying to finx some visaul requirements

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -8,7 +8,7 @@ const app = http.createServer((req, res) => {
   if (req.url === '/') {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('Hello Holberton School!');
-  }
+  } 
   else if (req.url === '/students') {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     countStudents(dbName)
@@ -19,10 +19,11 @@ const app = http.createServer((req, res) => {
         res.statusCode = 500;
         res.end('This is the list of our students\nCannot load the database');
       });
-  }
+  } 
   else {
     res.writeHead(404);
-    res.end();}
+    res.end(); 
+  }
 });
 
 app.listen(PORT);
