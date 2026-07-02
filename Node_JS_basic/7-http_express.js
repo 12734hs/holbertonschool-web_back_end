4498727c87d145e40be17190e1d1684c0bf655f3
Sabir <12734@holbertonstudents.com>
2026-07-02 17:04:07 +0400

    SOme Visual Fixes

--- Node_JS_basic/7-http_express.js
+++ Node_JS_basic/7-http_express.js
@@ -1,6 +1,6 @@
 const PORT = 1245;
 const express = require('express');
-const countStudents = require('./3-read_file_async')
+const countStudents = require('./3-read_file_async');
 const app = express();
 
 const dbName = process.argv[2];
@@ -11,13 +11,13 @@ app.get('/', (request, response) => {
 
 app.get('/students', (request, response) => {
   countStudents(dbName)
-      .then((result) => {
-        response.status(200).send(`This is the list of our students\n${result}`);
-      })
+    .then((result) => {
+      response.status(200).send(`This is the list of our students\n${result}`);
+    })
       .catch(() => {
         response.status(500).send('This is the list of our students\nCannot load the database');
       });
-})
+});
 
 app.listen(PORT);
 
