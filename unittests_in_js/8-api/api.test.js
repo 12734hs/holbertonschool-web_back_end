4c06259e401f68d474a18e4f989e4b118a4462f4
Sabir <12734@holbertonstudents.com>
2026-07-05 16:40:24 +0400

    Changed The Fetch Stuff in task

--- unittests_in_js/8-api/api.test.js
+++ unittests_in_js/8-api/api.test.js
@@ -1,27 +1,30 @@
-const request = require('supertest')
+const request = require('request')
 const { expect } = require('chai')
 
 const app = require('./api')
 
 describe('CHecking the backend system', () => {
     describe('GET /health', () => {
-        it('Check is the server alive or not', async() => {
-            const res = await request(app).get('/health')
-
-            expect(res.text).to.be.equal('server is alive!!!')
+        it('Check is the server alive or not', (done) => {
+            request('http://localhost:7865/health', (err, res, body) => {
+                expect(body).to.equal('server is alive!!!')
+                done();
+            })
         })
     })
     
     describe('GET /', () => {
-        it('check the status code', async () => {
-            const res = await request(app).get('/')
-
-            expect(res.statusCode).to.equal(200);
+        it('check the status code', (done) => {
+            request('http://localhost:7865', (err, res, bodu) => {
+                expect(res.statusCode).to.equal(200);
+                done();
+            })
         });
-        it('check the correst result', async () => {
-            const res = await request(app).get('/')
-
-            expect(res.text).to.be.equal('Welcome to the payment system');
+        it('check the correst result', (done) => {
+            request('http://localhost:7865', (err,res,body) => {
+                expect(body).to.equal('Welcome to the payment system')
+                done();
+            })
         })
     })
 })
