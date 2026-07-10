8c3f065fe14bb07a6c0632c49f0484fb0e9cbc23
Sabir <12734@holbertonstudents.com>
2026-07-10 21:29:42 +0400

    SOme visual fixes

--- Node_JS_basic/full_server/routes/index.js
+++ Node_JS_basic/full_server/routes/index.js
@@ -2,7 +2,7 @@ import express from 'express';
 import AppController from '../controllers/AppController';
 import StudentsController from '../controllers/StudentsController';
 
-const router = express.Router()
+const router = express.Router();
 
 router.get('/', AppController.getHomepage);
 router.get('/students', StudentsController.getAllStudents);
--- Node_JS_basic/full_server/server.js
+++ Node_JS_basic/full_server/server.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import routes from './routes'
+import routes from './routes';
 
 const PORT = 1245;
 const app = express();
@@ -7,7 +7,7 @@ const app = express();
 app.use('/', routes);
 
 app.listen(PORT, () => {
-    console.log(`Server use: ${PORT}`);
-})
+  console.log(`Server use: ${PORT}`);
+});
 
 export default app;
